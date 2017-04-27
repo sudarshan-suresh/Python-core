5e4be7dd72ebab3a151094703a18431eea39255d
Sudarshan Suresh <sudarshan.suresh@talentica.com>
2017-04-27 19:12:06 +0530

    check negative or positive

---
+++ core/CheckPosOrNeg.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+# Problem Statement :-
+# check whether a number is positive or negative
+
+# Task
+# 1. Get input from the user
+# 2. check it using if condition
+
+n = input("Enter a number")
+if n >= 0:
+    print("%d is Positive" % n)
+else:
+    print("%d Negative" % n)
